9628f593845732afbed743d73fa8b60cd5f72a74
DivineDemon <supame123@gmail.com>
2022-04-16 15:34:11 +0400

    Completed Navbar ✨

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,9 +7,17 @@ import logo from "../assets/logo.png";
 const isNotActiveStyle =
   "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
-  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
+  "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize";
 
-const Sidebar = ({ user, closeToggle }) => {
+const categories = [
+  { name: "Animals" },
+  { name: "Wallpapers" },
+  { name: "Photography" },
+  { name: "Gaming" },
+  { name: "Coding" },
+];
+
+const Sidebar = ({ closeToggle, user }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   };
@@ -30,15 +38,43 @@ const Sidebar = ({ user, closeToggle }) => {
             className={({ isActive }) =>
               isActive ? isActiveStyle : isNotActiveStyle
             }
+            onClick={handleCloseSidebar}
           >
             <RiHomeFill />
             Home
           </NavLink>
           <h3 className="mt-2 px-5 text-base 2xl:text-xl">
-            Discover categories
+            Discover cateogries
           </h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
         </div>
       </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img
+            src={user.image}
+            className="w-10 h-10 rounded-full"
+            alt="user-profile"
+          />
+          <p>{user.userName}</p>
+          <IoIosArrowForward />
+        </Link>
+      )}
     </div>
   );
 };
