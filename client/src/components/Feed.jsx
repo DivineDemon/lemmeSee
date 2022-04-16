8ac23111645c089913bf752bdef2aa62cd1e02e5
DivineDemon <supame123@gmail.com>
2022-04-16 16:25:26 +0400

    Added a Loader Animation ✨

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <Spinner message="We are adding new ideas to your Feed!" />;
+  }
   return <div>Feed</div>;
 };
 
