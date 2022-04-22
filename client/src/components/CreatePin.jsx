8a14572725b356083579c8bda24ec3b392068a7a
DivineDemon <supame123@gmail.com>
2022-04-23 02:47:38 +0400

    Added Dummy Data, Working on Creating Pins! 🔨

--- client/src/components/CreatePin.jsx
+++ client/src/components/CreatePin.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { client } from "../client";
+import Spinner from "./Spinner";
 
-const CreatePin = () => {
-  return <div>CreatePin</div>;
+// Categories Array Import
+import { categories } from "../utils/data";
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState("");
+  const [about, setAbout] = useState("");
+  const [destination, setDestination] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">
+          Please Fill in all the Fields!
+        </p>
+      )}
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-whiote lg:p-5 p-3 lg:w-4/5 w-full" />
+    </div>
+  );
 };
 
 export default CreatePin;
