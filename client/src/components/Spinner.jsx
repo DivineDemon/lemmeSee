cf604313b2f8bf92694c2c4e31edcd8bd61d1f42
DivineDemon <supame123@gmail.com>
2022-04-23 05:20:32 +0400

    Pin Details Done! ✨🔨

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import { Circles } from "react-loader-spinner";
 
 const Spinner = ({ message }) => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
+    <div className="flex flex-col justify-center items-center w-full h-full pt-5">
       <Circles color="#00BFFF" height={50} width={200} className="m-5" />
       <p className="text-lg text-center px-2">{message}</p>
     </div>
